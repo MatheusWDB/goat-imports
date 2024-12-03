81932ca21ea548bd3ec888429b55ecf52033adc4
MatheusWDB <matheuswdb1703@gmail.com>
2024-12-03 13:50:38 -0300

    .

--- pedidos/pedidos.js
+++ pedidos/pedidos.js
@@ -5,6 +5,7 @@ const urlLocal = "http://localhost:8080"
 const url = "https://goatimports.onrender.com"
 const content = document.getElementById("content")
 const modalContent = document.getElementById("modal-content")
+var products
 checkAuthUserId()
 // INICIA ANTES DE TUDO
 
@@ -57,7 +58,7 @@ function closemodal() {
 function renderizarPedidos() {
     content.innerHTML = ''
 
-    if (orders.length == 0){
+    if (orders.length == 0) {
         const vazio = document.createElement("h3")
         vazio.textContent = "Você ainda não finalizou nenhuma compra!"
 
@@ -105,9 +106,73 @@ function renderizarPedidos() {
     });
 }
 
-function selecionarPedido(order) {
+async function selecionarPedido(order) {
     openModal()
-    
+    const PedidosSelecionado = document.getElementById("PedidosSelecionado")
+    PedidosSelecionado.innerHTML = ''
+    products = []
+
+    try {
+        for (const product of order.products) {
+            document.getElementById('favicon').setAttribute('href', '../imagens/spinner.gif');
+            document.getElementById('loading-overlay').style.display = 'flex';
+
+            const response = await fetch(`${url}/products/findById/${product.idProduct}`, {
+                method: 'GET'
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                data.subTotal = product.subTotal;
+                data.quantity = product.quantity;
+                products.push(data);
+            } else {
+                const error = await response.json();
+                console.log(error);
+                alert(error.message);
+            }
+        }
+    } catch (error) {
+        console.log('Erro ao fazer a requisição: ', error);
+        alert('Erro no servidor! ' + error.message);
+    } finally {
+        document.getElementById('favicon').setAttribute('href', '../imagens/logo2.png');
+        document.getElementById('loading-overlay').style.display = 'none';
+    }
+
+    products.forEach(product => {
+        const img = document.createElement("img")
+        img.src = product.imgUrl
+        img.alt = product.name
+
+        PedidosSelecionado.appendChild(img)
+
+        const contentProduto = document.createElement("contentProduto")
+        contentProduto.classList.add("contentProduto")
+
+        const nome = document.createElement("p")
+        nome.textContent = product.name
+
+        contentProduto.appendChild(nome)
+
+        const quantidade = document.createElement("p")
+        quantidade.textContent = `Quantidade: ${product.quantity}`
+
+        contentProduto.appendChild(quantidade)
+
+        const valor = document.createElement("p")
+        valor.textContent = `Valor: R$ ${product.subTotal.toFixed(2).toString().replace(".",",")}`
+
+        contentProduto.appendChild(valor)
+
+        PedidosSelecionado.appendChild(contentProduto)
+    });
+
+    const valorTotal = document.createElement("h3")
+    valorTotal.textContent = `Total: R$ ${order.total.toFixed(2).toString().replace(".",",")}`
+    valorTotal.style.width = '200px'
+
+    PedidosSelecionado.appendChild(valorTotal)
 }
 
 function logout() {
